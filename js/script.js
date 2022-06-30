a811d6fbb689ee560b8c37662ea413adf93352b9
shogo61 <58659726+shogo61@users.noreply.github.com>
2022-06-30 10:18:43 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 $(function(){
+    // スタート画面
     $(".task").hide();
     $(".btns").hide();
 
@@ -9,23 +10,57 @@ $(function(){
 
         // 問題が上書きされないようにクラス付与
         for(var i=0;i<81;i++){
-        if(!$("td").eq(i).text()==""){
-            $("table tr td").eq(i).addClass("done");
+            if(!$("td").eq(i).text()==""){
+                $("table tr td").eq(i).addClass("done");
+            }
         }
-    }
     $("td").click(function(){
         
         //クリックされた地点の背景を変更
         $("td").removeClass("target");
-        $(this).addClass("target");
+        if(!$(this).hasClass("done")){// doneとtargetが同時に存在しないように
+            $(this).addClass("target");
+        }
         
         // ボタンが押されたらその値をtdに入力
-        //if文で問題が上書きされないようにする
         $("button").click(function(){
-            if(!$(".target").hasClass("done")){
-                var btn=$(this).index();
-                $(".target").html(btn);
+            var btn=$(this).index();
+            $(".target").html(btn);
+        })
+        $("html").keyup(function(e){
+            switch(e.which){
+                case 49:
+                    $(".target").html(1)
+                break;
+                case 50:
+                    $(".target").html(2)
+                break;
+                case 51:
+                    $(".target").html(3)
+                break;
+                case 52:
+                    $(".target").html(4)
+                break;
+                case 53:
+                    $(".target").html(5)
+                break;
+                case 54:
+                    $(".target").html(6)
+                break;
+                case 55:
+                    $(".target").html(7)
+                break;
+                case 56:
+                    $(".target").html(8)
+                break;
+                case 57:
+                    $(".target").html(9)
+                break;
+                case 48:
+                    $(".target").html(" ")
+                break;
             }
+            
         })
 
         //縦と横の値を毎回初期化
@@ -63,9 +98,10 @@ $(function(){
         };
         
         //3*3のブロックごとの計算
-        var start_i=0
-        var stop_i=3
-        while(cnt<3){
+        var start_i=0;
+        var stop_i=0;
+        var b_cnt=0
+        while(b_cnt<3){
             var sumb1=0;
             var sumb2=0;
             var sumb3=0;
@@ -83,6 +119,7 @@ $(function(){
             $(".block th").eq(0).html(sumb1);
             $(".block th").eq(1).html(sumb2);
             $(".block th").eq(2).html(sumb3);
+            b_cnt+=1;
         }
         
         //完了したかの判定
